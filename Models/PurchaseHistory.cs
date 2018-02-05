83726ae6601f15dc035cf101e1801616ce6ae652
iaholyk <iaholyk@gmail.com>
2018-02-05 12:17:02 -0800

    Pet classes and customer information
    Purchase history, Pets, and Kennel Visits.

---
+++ Models/PurchaseHistory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentitySample.Models
+{
+    public class PurchaseHistory
+    {
+        [Key]
+        [Required]
+        public int PurchaseId { get; set; }
+
+        [Display(Name = "Purchase Date")]
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime PurchaseDate { get; set; }
+
+        [Display(Name = "Package")]
+        [Required]
+        [DataType(DataType.Text)]
+        public string Package { get; set; }
+
+        [Display(Name = "Dog Name")]
+        [Required]
+        [DataType(DataType.Text)]
+        public string Dog { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
